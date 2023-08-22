fead558bebd66767911dcb42ecfb129677c61c91
Diego Frausto <136942042+DiegoFraR@users.noreply.github.com>
2023-08-22 17:02:29 -0400

    Update FizzBizz.java

--- FizzBizz.java
+++ FizzBizz.java
@@ -1,10 +1,10 @@
-//Simple program that writes Fizzbuzz everytime the number is divisible by 3 and 5. The program writes Fizz if the number is divisible by 3
+//Simple program that writes Fizzbuzz every time the number is divisible by 3 and 5. The program writes Fizz if the number is divisible by 3
 //The program writes buzz if the number is divisible by 5. Otherwise, it prints the number.
 
 public class FizzBizz {
     public static void main(String[]args){
 
-        for(int number = 1; number < 100; number++){
+        for(int number = 1; number <= 100; number++){
             if(number % 3 == 0 && number % 5 ==0){
                 System.out.println("Fizzbuzz");
             }
